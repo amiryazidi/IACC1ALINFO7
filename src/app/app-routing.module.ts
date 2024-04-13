6437d13fc240453be5ae1a9bb34e2f240b9ee47c
Amir YAZIDI <amir.yazidi1@esprit.tn>
2024-04-13 18:01:22 +0100

    formualaire

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //route par defaut
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'product', component:ProductComponent},
+  {path:'login', component:ReactiveFormComponent},
+  {path:'detailP/:id', component:DetailProductComponent},
+  //route not found
+  {path:'**',component:NotFoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductComponent } from './product/product.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    NavbarComponent,
+    FooterComponent,
+    NotFoundComponent,
+    DetailProductComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
